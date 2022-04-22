0709ede71999cfa1810f58928d7fbd0665986202
eritkd <102778281+eritkd@users.noreply.github.com>
2022-04-22 06:53:33 +0000

    [Add] 4-7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.hosts << "891a130e57ad45ad89c09c0fabea0bd4.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
